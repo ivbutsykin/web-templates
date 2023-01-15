1c346ed8ec0d4a4b7de5ba2a73a1072775ee9299
ivbutsykin <ivbutsykin@gmail.com>
2023-01-15 22:44:07 +0200

    feat: add pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" rel="noreferrer" target="_blank">
